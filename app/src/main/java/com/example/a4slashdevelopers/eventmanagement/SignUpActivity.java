76f8394e53f4b80a124cd71a353f1b69961f0cbc
Abdul Wahab <mwahab@live.com>
2018-01-03 17:07:25 +0500

    password functionality add

--- app/src/main/java/com/example/a4slashdevelopers/eventmanagement/SignUpActivity.java
+++ app/src/main/java/com/example/a4slashdevelopers/eventmanagement/SignUpActivity.java
@@ -15,6 +15,22 @@ import java.util.regex.Pattern;
 public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    public static String message="/**\n" +
+            "\t  * returns true if the provided password\n" +
+            "\t  * \n" +
+            "\t  * \t- contains at least 1 digit\n" +
+            "\t  * \t- contains at least 1 lowercase letter\n" +
+            "\t  * \t- contains at least 1 uppercase letter\n" +
+            "\t  * \t- contains at least 1 of the special \n" +
+            "\t  * \t\tcharacters !@#$%^&*()_+\\-=[]{};':\"\\|,.<>/?\n" +
+            "\t  * \t- is at least 6 character long\n" +
+            "\t  * \t- is at most 15 characters long  \n" +
+            "\t  * \t(note : for best practices you could even set \n" +
+            "\t  * \tthe max length to a higher value ) \n" +
+            "\t  * \n" +
+            "\t  * @param password the password to validate\n" +
+            "\t  * @return true if the password validates\n" +
+            "\t  */";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +43,7 @@ public class SignUpActivity extends AppCompatActivity {
     {
 
         isEmailValidate( ((EditText)findViewById(R.id.emailidsignup)).getText().toString().trim());
-
+        ispasswordValidate(((EditText)findViewById(R.id.passwordidsignup)).getText().toString().trim());
     }
     void isEmailValidate(String email)
     {
@@ -40,6 +56,33 @@ public class SignUpActivity extends AppCompatActivity {
             }
             else
                 Toast.makeText(this, "valid Email", Toast.LENGTH_SHORT).show();
+
+    }
+    void ispasswordValidate(String password){
+
+        final String PASSWORD_REGEXP =
+                "^"
+                        + "(?=.*\\d)"
+                        + "(?=.*[a-z])"
+                        + "(?=.*[A-Z])"
+                        + "(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])"
+                        + "."
+                        + "{6,15}"
+                        + "$";
+
+         Pattern pattern = Pattern.compile(PASSWORD_REGEXP);
+         Matcher matcher;
+
+        matcher = pattern.matcher(password);
+         if (matcher.matches())
+         {
+             Toast.makeText(this, "valid", Toast.LENGTH_SHORT).show();
+         }
+         else
+         {
+             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+         }
 
     }
 }
