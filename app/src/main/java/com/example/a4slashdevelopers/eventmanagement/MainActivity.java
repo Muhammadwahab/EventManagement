caa47ebc302475dd612b25c3bd34cb30d614754f
Abdul Wahab <mwahab@live.com>
2018-01-04 15:05:04 +0500

    Sign in functionality add

--- app/src/main/java/com/example/a4slashdevelopers/eventmanagement/MainActivity.java
+++ app/src/main/java/com/example/a4slashdevelopers/eventmanagement/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.a4slashdevelopers.eventmanagement;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.a4slashdevelopers.eventmanagement.UTIL.utilityClass;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -38,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
     public void loginonclick(View view)
     {
          signin(((EditText)(findViewById(R.id.emailidsignin))).getText().toString().trim(), ((EditText)(findViewById(R.id.passwordidsignin))).getText().toString().trim());
-
     }
 
     private void signin(String email, String password) {
@@ -47,8 +51,45 @@ public class MainActivity extends AppCompatActivity {
         else
         {
             // valid int a
+            if (passwordValidation(password)==true)
+                // firebase sign in method call
+                firebaseEmailAndPasswordSignin(email,password);
+                else
+            Toast.makeText(this, "Password Should Be Equal To And Greator Then 6", Toast.LENGTH_SHORT).show();
+
         }
 
 
+    }
+
+    private void firebaseEmailAndPasswordSignin(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Signin", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "User Sign is Successfully"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Signin", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private boolean passwordValidation(String password) {
+        if (password.length()>=6)
+            return true;
+        else
+            return false;
     }
 }
